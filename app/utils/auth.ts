ea24f44675ae435ab58c0514a0614b631d4b31e1
yashdev9274 <yashdev.yvd@gmail.com>
2025-02-09 17:46:18 +0530

    Add Nodemailer provider to NextAuth and update dependencies.

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,5 +1,19 @@
 import NextAuth from "next-auth"
+import Nodemailer from "next-auth/providers/nodemailer"
  
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Nodemailer({
+        server: {
+          host: process.env.EMAIL_SERVER_HOST,
+          port: process.env.EMAIL_SERVER_PORT,
+          auth: {
+            user: process.env.EMAIL_SERVER_USER,
+            pass: process.env.EMAIL_SERVER_PASSWORD,
+          },
+        },
+        from: process.env.EMAIL_FROM,
+      }),
+  ],
 })
