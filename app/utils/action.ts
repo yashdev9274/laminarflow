784402faf8ff2661e0561edd8d02f9cda45359b4
yashdev9274 <yashdev.yvd@gmail.com>
2025-02-22 18:16:15 +0530

    Implement onboarding form with validation and user update functionality.

--- app/utils/action.ts
+++ app/utils/action.ts
@@ -1,7 +1,32 @@
 'use server'
 
+import { redirect } from "next/navigation";
+import { prisma } from "./db";
 import { requireUser } from "./requireAuth"
+import { onboardingUserSchema } from "./zodSchema";
+import {parseWithZod} from "@conform-to/zod"
 
-export default async function onboarduser(){
+export default async function onboardUser(prevState: any, formData: FormData){
     const session = await requireUser(); 
+
+    const submission = parseWithZod(formData,{
+        schema: onboardingUserSchema,
+    });
+
+    if(submission.status !== "success"){
+        return submission.reply();
+    }
+
+    const data = await prisma.user.update({
+        where:{
+            id: session.user?.id,
+        },
+        data:{
+            firstName: submission.value.firstName,
+            lastName: submission.value.lastName,
+            address: submission.value.address,
+        },
+    });
+
+    return redirect('/dashboard');
 }
