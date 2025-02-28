65491160e8517ff835f0c8f14cb083bb37d13d4b
yashdev9274 <yashdev.yvd@gmail.com>
2025-02-28 14:13:02 +0530

    Add invoice status selection to CreateInvoiceSheet component.

--- components/createInvoiceSheetButton.tsx
+++ components/createInvoiceSheetButton.tsx
@@ -17,7 +17,7 @@ import { CalendarX2Icon, PlusIcon } from "lucide-react"
 import { Card, CardContent } from "./ui/card"
 import { Badge } from "./ui/badge"
 import { Select, SelectContent, SelectItem } from "./ui/select"
-import { SelectTrigger, SelectValue } from "@radix-ui/react-select"
+import { SelectGroup, SelectTrigger, SelectValue } from "@radix-ui/react-select"
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
 import { useState } from "react"
 import { Calendar } from "./ui/calendar"
@@ -37,112 +37,139 @@ export function CreateInvoiceSheet() {
             <SheetContent className="w-full max-w-4xl mx-auto">
                 <SheetHeader>
                 <SheetTitle>Create Invoices</SheetTitle>
-                <Card className="w-full max-w-4xl mx-auto ">
-                    <CardContent className="p-6">
-                        <div className="flex flex-col gap-1 w-fit mb-6">
-                            <div className="flex items-center gap-4">
-                                <Badge variant='secondary'>Draft</Badge>  
-                                <Input
-                                    placeholder="Invoice Title"
-                                />      
+                    <Card className="w-full max-w-4xl mx-auto ">
+                        <CardContent className="p-6">
+                            <div className="flex flex-col gap-1 w-fit mb-6">
+                                <div className="flex items-center gap-4">
+                                    <Badge variant='secondary'>Draft</Badge>  
+                                    <Input
+                                        placeholder="Invoice Title"
+                                    />      
+                                </div>
                             </div>
-                        </div>
-                        <div className="grid md:grid-cols-3 gap-6 mb-6">
-                            <div>
-                                <Label>Invoice No.</Label>
+                            <div className="grid md:grid-cols-3 gap-6 mb-6">
                                 <div>
-                                    <Input
-                                        placeholder="00"
-                                        className="rounded-1-none"
-                                    />
-                                </div>
-                            </div>
-                            <div>
-                                <Label>Currency</Label>
-                                <Select
-                                    defaultValue='INR'
-                                >
-                                    <SelectTrigger>
-                                        <SelectValue placeholder='Select Currency'/>
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem value='INR'>INR</SelectItem>
-                                        <SelectItem value='USD'>USD</SelectItem>
-                                        <SelectItem value='EUR'>EUR</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </div>
-                        </div>
-
-                        {/* User and Client Details */}
-                        <div className="grid md:grid-cols-2 gap-6 mb-6">
-                            
-                            {/* User Details */}
-                            <div>
-                                <Label>From:</Label>
-                                <div className="space-y-2">
-                                    <Input
-                                        placeholder="Your Name"
-                                    />
-                                    <Input
-                                        placeholder="Your Email"
-                                    />
-                                    <Input
-                                        placeholder="Your Address"
-                                    />
-                                </div>
-                            </div>
-
-                            {/* Client Details */}
-                            <div>
-                                <Label>To:</Label>
-                                <div className="space-y-2">
-                                    <Input
-                                        placeholder="Client Name"
-                                    />
-                                    <Input
-                                        placeholder="Client Email"
-                                    />
-                                    <Input
-                                        placeholder="Client Address"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-
-                        {/* Calendar */}
-                        <div className="grid md:grid-cols-2 gap-6 mb-6">
-                            <div className="flex items-center gap-2">
-                                <Label>Date:</Label>
-                                <Popover>
-                                    <PopoverTrigger asChild>
-                                        <Button
-                                            variant="outline"
-                                            className="w-17 text-left justify-center mr-5"
-                                        >
-                                            <CalendarX2Icon/>
-                                            {selectedDate ? (
-                                                new Intl.DateTimeFormat("en-US", {
-                                                    dateStyle: "short",
-                                                }).format(selectedDate)
-                                                ) : (
-                                                <span>Pick a Date</span>
-                                            )}
-                                        </Button>
-                                    </PopoverTrigger>
-                                    <PopoverContent>
-                                        <Calendar
-                                            selected = {selectedDate}
-                                            onSelect={(date: any)=>setSelectedDate(date || new Date())}
-                                            mode="single"
-                                            fromDate={new Date()}
+                                    <Label>Invoice No.</Label>
+                                    <div>
+                                        <Input
+                                            placeholder="00"
+                                            className="rounded-1-none"
                                         />
-                                    </PopoverContent>
-                                </Popover>
+                                    </div>
+                                </div>
+                                <div>
+                                    <Label>Currency</Label>
+                                    <Select
+                                        defaultValue='INR'
+                                    >
+                                        <SelectTrigger>
+                                            <SelectValue placeholder='Select Currency'/>
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value='INR'>INR</SelectItem>
+                                            <SelectItem value='USD'>USD</SelectItem>
+                                            <SelectItem value='EUR'>EUR</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
                             </div>
-                        </div>
-                    </CardContent>
-                </Card>
+
+                            {/* User and Client Details */}
+                            <div className="grid md:grid-cols-2 gap-6 mb-6">
+                                
+                                {/* User Details */}
+                                <div>
+                                    <Label>From:</Label>
+                                    <div className="space-y-2">
+                                        <Input
+                                            placeholder="Your Name"
+                                        />
+                                        <Input
+                                            placeholder="Your Email"
+                                        />
+                                        <Input
+                                            placeholder="Your Address"
+                                        />
+                                    </div>
+                                </div>
+
+                                {/* Client Details */}
+                                <div>
+                                    <Label>To:</Label>
+                                    <div className="space-y-2">
+                                        <Input
+                                            placeholder="Client Name"
+                                        />
+                                        <Input
+                                            placeholder="Client Email"
+                                        />
+                                        <Input
+                                            placeholder="Client Address"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="grid md:grid-cols-2 gap-6 mb-6">
+                                
+                                {/* Calendar */}
+
+                                <div className="flex items-center gap-2">
+                                    <Label>Date:</Label>
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <Button
+                                                variant="outline"
+                                                className="w-17 text-left justify-center mr-5"
+                                            >
+                                                <CalendarX2Icon/>
+                                                {selectedDate ? (
+                                                    new Intl.DateTimeFormat("en-US", {
+                                                        dateStyle: "short",
+                                                    }).format(selectedDate)
+                                                    ) : (
+                                                    <span>Pick a Date</span>
+                                                )}
+                                            </Button>
+                                        </PopoverTrigger>
+                                        <PopoverContent>
+                                            <Calendar
+                                                selected = {selectedDate}
+                                                onSelect={(date: any)=>setSelectedDate(date || new Date())}
+                                                mode="single"
+                                                fromDate={new Date()}
+                                            />
+                                        </PopoverContent>
+                                    </Popover>
+                                </div>
+
+                            </div>
+                                {/* Invoice Status */}
+                                
+                                <div >
+                                    <Label>Invoice Status:</Label>
+                                    <Select>
+                                        <SelectTrigger className="w-17 text-left justify-center mr-5 gap-2">
+                                            <SelectValue 
+                                                placeholder='Due'
+                                                className="w-17 text-left justify-center mr-5 gap-2"
+                                            />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectItem value='0'>Due</SelectItem>
+                                                <SelectItem value='Paid'>Paid</SelectItem>
+                                                <SelectItem value='Sent'>Sent</SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+
+                            {/*  */}
+
+                            <div></div>
+                        </CardContent>
+                    </Card>
                 </SheetHeader>
                 <SheetFooter className="flex justify-end mt-5">
                     <SheetClose asChild>
