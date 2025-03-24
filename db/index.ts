0d2ffc941f915f02e229e80ba6d8cf8772409586
yashdev9274 <yashdev.yvd@gmail.com>
2025-03-24 14:02:46 +0530

    Remove Prisma client initialization from db/index.ts

--- db/index.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-export const prisma: PrismaClient = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") global.prisma = prisma;
