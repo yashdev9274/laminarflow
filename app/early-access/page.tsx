42c667c9c38c75434677f8dc2689509ed4b6500d
yashdev9274 <yashdev.yvd@gmail.com>
2025-03-26 14:31:50 +0530

    Update buttons ui, add bg grid patter in Early-access page.

--- app/early-access/page.tsx
+++ app/early-access/page.tsx
@@ -11,6 +11,10 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 
 import { Input } from "@/components/ui/input"
+import { AnimatedGridPattern } from "@/components/earlyAccess/sqaureBgGrid"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+import RedirectButton from "../components/redirectButton"
 
 
 export default function EarlyAccess(){
@@ -47,8 +51,21 @@ export default function EarlyAccess(){
 
    return(
       <div className="flex flex-col items-center justify-center min-h-screen">
-         <Card className="mt-4 w-full border-none bg-transparent shadow-none">
+        <div>
+            <header className="absolute left-4 top-4 md:left-8 md:top-8">
+                    <div className="flex justify-between items-center container">                    
+                        <RedirectButton text="Back" href = "/"/>
+                    </div>
+            </header>
+        </div>
+        
+        <Card className="mt-4 w-full border-none bg-transparent shadow-none">
                 <CardContent className="flex flex-col items-center justify-center px-0">
+                    <p>
+                        <span className="text-4xl font-semibold leading-tight tracking-[-0.03em] text-white sm:text-6xl md:px-0">
+                            Become our Early User!
+                        </span>
+                    </p>
                 <div className="flex items-center justify-center gap-4 mt-7">
                     <form
                         action={action}
@@ -75,6 +92,16 @@ export default function EarlyAccess(){
                 </div>
                 </CardContent>
             </Card>
+            <AnimatedGridPattern
+                numSquares={30}
+                maxOpacity={0.1}
+                duration={3}
+                repeatDelay={1}
+                className={cn(
+                "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+                "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+                )}
+            />
       </div>
    )
 }
