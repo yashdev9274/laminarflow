555ffccc3bc4879eda5dcc00c9327499cbc69ce0
yashdev9274 <yashdev.yvd@gmail.com>
2025-04-01 14:00:29 +0530

    Enhance CompaniesTable component with TableBody, TableCaption, and sample data rows.

--- components/dashboard/companies/CompaniesTable.tsx
+++ components/dashboard/companies/CompaniesTable.tsx
@@ -1,4 +1,4 @@
-import { Table, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCaption, TableCell, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function CompaniesTable(){
    return(
@@ -16,6 +16,19 @@ export default function CompaniesTable(){
                   <th className="text-right">Action</th>
                </TableRow>
             </TableHeader>
+            <TableBody>
+               <TableRow>
+                  <TableCell className="text-left">1</TableCell>
+                  <TableCell className="text-left">Air Bnb</TableCell>
+                  <TableCell className="text-left">airbnb.com</TableCell>
+                  <TableCell className="text-left">Yash</TableCell>
+                  <TableCell className="text-left">12/12/2021</TableCell>
+                  <TableCell className="text-left">5,000</TableCell>
+                  <TableCell className="text-left">888 Brannan St</TableCell>
+                  <TableCell className="text-right">Edit</TableCell>
+               </TableRow>
+            </TableBody>
+            <TableCaption className="mt-5"> Record of your transactions.</TableCaption>
          </Table>
       </div>
    )
