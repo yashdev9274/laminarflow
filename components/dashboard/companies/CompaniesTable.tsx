9658d20782dd770516a94a403ab657bfac3d3522
yashdev9274 <yashdev.yvd@gmail.com>
2025-04-01 13:44:16 +0530

    Enhance CompaniesTable component with structured table layout and headers.

--- components/dashboard/companies/CompaniesTable.tsx
+++ components/dashboard/companies/CompaniesTable.tsx
@@ -1,7 +1,22 @@
+import { Table, TableHeader, TableRow } from "@/components/ui/table";
+
 export default function CompaniesTable(){
    return(
-      <div>
-         Companies Table
+      <div className="overflow-x-auto ml-2 min-h-[450px]">
+         <Table className="min-w-full">
+            <TableHeader>
+               <TableRow>
+                  <th className="text-left">Serial No.</th>
+                  <th className="text-left">Name</th>
+                  <th className="text-left">Domain Name</th>
+                  <th className="text-left">Account Owner</th>
+                  <th className="text-left">Creation Date</th>
+                  <th className="text-left">Employees</th>
+                  <th className="text-left">Address</th>
+                  <th className="text-right">Action</th>
+               </TableRow>
+            </TableHeader>
+         </Table>
       </div>
    )
 }
