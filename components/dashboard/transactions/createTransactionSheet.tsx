70f257c60832082ff098057b8c61531391e981f7
yashdev9274 <yashdev.yvd@gmail.com>
2025-04-07 23:13:28 +0530

    Refactor CreateTransactionSheet component to integrate Sheet for improved transaction management UI.

--- components/dashboard/transactions/createTransactionSheet.tsx
+++ components/dashboard/transactions/createTransactionSheet.tsx
@@ -1,12 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { PlusIcon } from "lucide-react";
 
 export async function CreateTransactionSheet(){
    return(
-      <div className="flex items-center justify-end">
-         <Button variant="secondary" className="w-4 h-6">
-            <PlusIcon/>
-         </Button>
+      <div className="flex items-center justify-center w-11">
+
+         <Sheet>
+            <SheetTrigger asChild>
+               <Button variant="secondary" className="w-4 h-6">
+                  <PlusIcon/>
+               </Button>
+            </SheetTrigger>
+            <SheetContent>
+               <SheetHeader>
+                  <SheetTitle>Transactions</SheetTitle>
+               </SheetHeader>
+            </SheetContent>
+         </Sheet>
+         
       </div>
    )
 }
