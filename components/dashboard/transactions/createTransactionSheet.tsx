f16a7ac685321a503e1da3ea7173b5f1cf347159
yashdev9274 <yashdev.yvd@gmail.com>
2025-04-08 14:03:15 +0530

    Enhance CreateTransactionSheet component with Card layout and input for transaction amount.

--- components/dashboard/transactions/createTransactionSheet.tsx
+++ components/dashboard/transactions/createTransactionSheet.tsx
@@ -1,4 +1,7 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { PlusIcon } from "lucide-react";
 
@@ -12,9 +15,30 @@ export async function CreateTransactionSheet(){
                   <PlusIcon/>
                </Button>
             </SheetTrigger>
-            <SheetContent>
+            <SheetContent className="w-full max-w-7xl mx-">
                <SheetHeader>
                   <SheetTitle>Transactions</SheetTitle>
+
+                  <Card className="w-full">
+                     <CardContent className="p-6">
+                        <form
+                           className="grid gap-4"
+                        >
+                           <div className="flex flex-col gap-1 w-fit mb-6">
+                              <div className="flex items-center gap-4">
+                                 <Badge variant='secondary'>Amount</Badge>
+                                 <div className="space-y-1">
+
+                                    <Input
+                                       placeholder="$200"
+                                       className="text-4xl font-medium border-spacing-1 px-2 h-auto"
+                                    />
+                                 </div>
+                              </div>
+                           </div>
+                        </form>
+                     </CardContent>
+                  </Card>
                </SheetHeader>
             </SheetContent>
          </Sheet>
