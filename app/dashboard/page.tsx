0e9e7b462df9b4ab0be28bc010b617fbf8264d5d
yashdev9274 <yashdev.yvd@gmail.com>
2025-04-17 22:43:26 +0530

    Add DashboardBlocks and DashboardEmptyState components; implement data fetching and display logic in Dashboard page.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,49 @@
+import { DashboardBlocks } from "@/components/dashboard/analytics/dashboardBlocks";
 import SubmitButton from "../components/submitButton"
 import { signOut } from "../utils/auth"
+import { prisma } from "../utils/db";
 import { requireUser } from "../utils/requireAuth"
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
+import { DashboardEmptyState } from "@/components/dashboard/emptyStates/dashboardEmptystate";
+
+
+async function getData(userId: string) {
+    const data = await prisma.invoice.findMany({
+        where: {
+            userId: userId,
+        },
+        select: {
+            id: true,
+        },
+        });
+
+    return data;
+}
 
 export default async function Dashboard() {
 
-    const session = await requireUser() 
-    return (
-        <div className="min-h-screen flex items-center justify-center">
-            <h1>Dashboard</h1>
-            <form
-                action={async () => {
-                "use server"
-                await signOut()
-                }}
-            >
 
-            <SubmitButton text="LogOut"/> 
-            </form>
-        </div>
+
+    const session = await requireUser() 
+    const data = await getData(session.user?.id as string)
+    return (
+        <>
+        {data.length < 1 ? (
+            <DashboardEmptyState
+                title="No invoices found"
+                description="Create an invoice to see it right here"
+                buttontext="Create Invoice"
+                href="/dashboard/invoices/"
+            />
+        ) : (
+            <Suspense fallback={<Skeleton className="w-full h-full flex-1" />}>
+            <DashboardBlocks/>
+            <div className="grid gap-4 lg:grid-cols-3 md:gap-8">
+                
+            </div>
+            </Suspense>
+        )}
+    </>
     )
 }
