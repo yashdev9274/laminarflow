1de25ae2018c34a3f6ec61545691e409c7c9e192
yashdev9274 <yashdev.yvd@gmail.com>
2025-04-17 22:20:43 +0530

    Add CompaniesInfo and InvoiceInfo components; integrate into respective pages.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,6 +1,8 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { CreateInvoiceSheet } from "@/components/dashboard/invoice/createInvoiceSheetButton";
 import { InvoiceTable } from "@/components/dashboard/invoice/InvoiceTable";
+import InvoiceInfo from "@/components/dashboard/invoice/invoiceInfo";
+import { ReceiptIndianRupee } from "lucide-react";
 
 
 export default function InvoicePage(){
@@ -11,7 +13,11 @@ export default function InvoicePage(){
                     <div className="flex items-center justify-between ml-2">
                         <div>
                             <CardTitle className="text-lg font-semibold"> 
-                                Invoices
+                                <div className="flex items-center gap-4">
+                                    <ReceiptIndianRupee/>
+                                    Invoices
+                                    <InvoiceInfo/>
+                                </div>
                             </CardTitle>
                             <CardDescription className="text-sm">
                                 You can track your invoices here
