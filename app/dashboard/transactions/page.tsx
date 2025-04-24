733dfb50261ab9f6d79883bcbfedf92244952068
yashdev9274 <yashdev.yvd@gmail.com>
2025-04-24 12:34:36 +0530

    Add transaction analytics components and enhance transaction dashboard.

--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -1,10 +1,33 @@
+import { prisma } from "@/app/utils/db";
+import { requireUser } from "@/app/utils/requireAuth";
+import { TransactionsStatsBlocks } from "@/components/dashboard/analytics/transactionsStatsBlcoks";
+import { DashboardEmptyState } from "@/components/dashboard/emptyStates/dashboardEmptystate";
 import { CreateTransactionSheet } from "@/components/dashboard/transactions/createTransactionSheet";
 import TransactionInfo from "@/components/dashboard/transactions/transactionInfo";
 import { TransactionTable } from "@/components/dashboard/transactions/TransactionTable";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { BadgeIndianRupee } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { Suspense } from "react";
 
-export default function Transactions(){
+async function getData(userId: string){
+   const data = await prisma.transactions.findMany({
+      where:{
+         userId: userId,
+      },
+      select:{
+         id:true,
+      }
+   })
+   return data
+}
+
+
+export default async function Transactions(){
+
+   const session = await requireUser();
+   const data = await getData(session.user?.id as string)
    return(
       <div className="flex flex-col gap-4 p-4 pt-0">
          <Card>
@@ -26,9 +49,23 @@ export default function Transactions(){
                   <CreateTransactionSheet/>
                </div>
             </CardHeader>
-            <CardContent>
-               <TransactionTable/>
-            </CardContent>
+            <Separator/>
+            {data.length < 1 ? (
+               <DashboardEmptyState
+                  title="No transactions found"
+                  description="Create a transaction to see it right here"
+                  buttontext="Create Transaction"
+                  href="/dashboard/transactions/"
+               />
+            ) : (
+               <Suspense fallback={<Skeleton className="w-full h-full flex-1" />}>
+                  <TransactionsStatsBlocks/>
+                  <Separator className="mb-5"/>
+                  <CardContent className="mt-5">
+                     <TransactionTable/>
+                  </CardContent>
+               </Suspense>
+            )}
          </Card>
       </div>
    )
