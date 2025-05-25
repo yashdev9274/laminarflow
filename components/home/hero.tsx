c525e56431d5a2980dd99650fa253422c3a7d9a3
yashdev9274 <yashdev.yvd@gmail.com>
2025-05-25 18:52:19 +0530

    Update hero section

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -10,30 +10,9 @@ import Waitlist from "./waitlist"
 export default function Hero(){
 
     return(
-        <div className="mt-7">
-            <header className="sticky border rounded border-[#565555] bg-neutral-900/60 top-0 mb-5 mx-auto backdrop-blur">
-            {/* you can replace sticky -> relative */}
-                {/* <div className="flex justify-between items-center container"> */}
-                    {/* <Link href='/' className='relative mr-6 flex items-center space-x-2'> */}
-                        {/* <span className="text-2xl font-bold px-10 text-white">LF</span> */}
-                        {/* <Image
-                            src="/LF-logo.png"
-                            alt="LaminarFlow Logo"
-                            width={50}
-                            height={50}
-                        /> */}
-                    {/* </Link> */}
-                {/* <TalkToFounder text="Talk to Founder" redirectTo="https://cal.com/yash-dewasthale/talk-finance-management-with-lf-global" className="align-right rounded" /> */}
-                    {/* <div className="flex justify-between items-center mr-10"> */}
-                        {/* <RedirectButton text="Contact for Demo" href = "https://cal.link/LF-Founder-chat" className="align-right rounded" /> */}
-                    {/* </div> */}
-{/*  */}
-                {/* </div> */}
-                <NavbarDemo/>
-            </header>
-
-            <IntroBar text="LaminarFlow v0.1 Public Launch" href="https://x.com/dew_yashtwt/status/1920101314387673094"/>
-            <div>
+        <section className="py-24">
+            
+            <div className="container">
                 <p className="text-center text-sm font-semibold leading-tight tracking-[-0.03em]  text-zinc-400 sm:text-6xl md:px-0">The #1 </p>
                 <p className="text-center text-4xl font-bold leading-tight tracking-[-0.03em]  text-white sm:text-6xl md:px-0"> 
                     Open-Sourced <TypingText text="Fintech" cursor cursorClassName="h-9"/>
@@ -43,12 +22,12 @@ export default function Hero(){
             <div className="grid grid-col mx-auto w-full max-w-4xl">
                 <Balancer className="text-shinyGray mx-auto mt-3 mb-5 text-center text-[15px] leading-tight sm:text-[22px] ">
                     Manage your business with <span className="font-mono">LaminarFlow</span> – a
-                    modern, powerful, and affordable platform to manage your business' financial workflow
+                    modern, powerful, and affordable platform to manage your business&apos;s financial workflow
                 </Balancer>
                 {/* <RedirectButton text="Join Waitlist" href='/early-access'/> */}
                 <Waitlist/>
             </div>
             
-        </div>
+        </section>
     )
 }
---
+++ components/navbar/newNavbar.tsx
@@ -0,0 +1,27 @@
+import { NavbarDemo } from "./navbar";
+
+export default function NewNavbar() {
+   return (
+
+         <header className="sticky border rounded border-[#565555] bg-neutral-900/60 top-0 mt-7 mb-5 mx-auto backdrop-blur">
+               {/* you can replace sticky -> relative */}
+                  {/* <div className="flex justify-between items-center container"> */}
+                     {/* <Link href='/' className='relative mr-6 flex items-center space-x-2'> */}
+                           {/* <span className="text-2xl font-bold px-10 text-white">LF</span> */}
+                           {/* <Image
+                              src="/LF-logo.png"
+                              alt="LaminarFlow Logo"
+                              width={50}
+                              height={50}
+                           /> */}
+                     {/* </Link> */}
+                  {/* <TalkToFounder text="Talk to Founder" redirectTo="https://cal.com/yash-dewasthale/talk-finance-management-with-lf-global" className="align-right rounded" /> */}
+                     {/* <div className="flex justify-between items-center mr-10"> */}
+                           {/* <RedirectButton text="Contact for Demo" href = "https://cal.link/LF-Founder-chat" className="align-right rounded" /> */}
+                     {/* </div> */}
+   {/*  */}
+                  {/* </div> */}
+                  <NavbarDemo/>
+         </header>
+   )
+}
