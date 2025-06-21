50c70dfbfa2ccdee0e2c2e7ca6ac01d057c67fb1
yashdev9274 <yashdev.yvd@gmail.com>
2025-06-21 16:14:54 +0530

    Add PaymentTab component and integrate into PaymentProvider page: - Created a new PaymentTab component to manage payment options with tabs for Polar, Stripe, and DodoPayment. - Integrated PaymentTab into the PaymentProvider page, replacing the previous card layout for a more organized tabbed interface. - Added necessary imports and adjusted layout for improved user experience.

--- app/dashboard/payment-provider/page.tsx
+++ app/dashboard/payment-provider/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
+import BackRedirectButton from "@/app/components/backRedirectButton";
+import PaymentTab from "@/app/components/paymentTabs";
 
 export default function PaymentProvider(){
    return(
       <main className="relative mx-2 mb-10 mt-4 space-y-8 overflow-hidden px-1 sm:mx-3 md:mx-5 md:mt-5 lg:mx-7 lg:mt-8 xl:mx-10">
+         <div className="mb-4 flex items-center justify-between md:mb-8 lg:mb-12">   
+            <BackRedirectButton
+               href="/dashboard"
+               text="Back to dashboard"
+            />
+         </div>
+         
          <header>
             <section className="mb-4 flex items-center justify-between md:mb-8 lg:mb-12">
                <div className="space-y-1">
@@ -15,32 +21,11 @@ export default function PaymentProvider(){
                </div>
             </section>
          </header>
-         <div>
-            <Card>
-               <CardHeader>
-                  <div className="flex items-center justify-between mb-4">
-                     <div className="space-y-1">
-                        <CardTitle>Polar</CardTitle>
-                        <CardDescription>Connect with your polar account.</CardDescription>
-                     </div>
-                     <Button variant="secondary" className="bg-blue-800 text-neutral-200 hover:bg-blue-700 font-bold py-2 px-4 rounded">
-                        Polar Sh
-                     </Button>
-                  </div>
-                  <Separator/>
-                  <div className="flex items-center justify-between mt-4 mb-4">
-                     <div className="space-y-1">
-                        <CardTitle>Stripe</CardTitle>
-                        <CardDescription>Connect with your stripe account.</CardDescription>
-                     </div>
-                     <Button variant="secondary" className="bg-blue-800 text-neutral-200 hover:bg-blue-700 font-bold py-2 px-4 rounded">
-                        Stripe
-                     </Button>
-                  </div>
-                  <Separator/>
-               </CardHeader>
-            </Card>
+         <div className="mb-4 flex items-center justify-center md:mb-8 lg:mb-12">
+            <PaymentTab/>
          </div>
+
+         
       </main>
    )
 }
