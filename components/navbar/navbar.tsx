de44e5e242ae1a86ada8a47a0af3fb885517ff44
yashdev9274 <yashdev.yvd@gmail.com>
2025-06-26 16:02:35 +0530

    Small fix

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -60,7 +60,7 @@ export function NavbarDemo() {
         <NavBody>
           <NavbarLogo />
           <NavItems items={navItems} />
-          <div className="flex items-center gap-4 ml-5">
+          <div className="flex items-center gap-4 ml-7">
             <NavbarButton className="bg-hidden text-white" variant="secondary">
               <Link
                 href={session?'/dashboard':"/login"}
