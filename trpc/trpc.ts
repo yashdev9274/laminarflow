5b7db82cc2a9832a2c1e0121d7f14df3c1e5fa9c
yashdev9274 <yashdev.yvd@gmail.com>
2025-07-19 21:00:17 +0530

    Add OpenAI integration and enhance dashboard layout: - Added OpenAI package to package.json for AI functionalities. - Introduced ChatWrapper and ChatInput components for chat interface. - Implemented SidebarRight component to integrate chat functionality into the dashboard. - Refactored DashboardLayout to accommodate new sidebar elements and improve layout structure. - Updated TRPC to include user financial data fetching for enhanced user experience.

--- trpc/trpc.ts
+++ trpc/trpc.ts
@@ -1,14 +1,25 @@
-import { initTRPC } from '@trpc/server';
- 
-/**
- * Initialization of tRPC backend
- * Should be done only once per backend!
- */
+import  { auth }  from '@/app/utils/auth';
+import { initTRPC, TRPCError } from '@trpc/server';
+
 const t = initTRPC.create();
- 
-/**
- * Export reusable router and procedure helpers
- * that can be used throughout the router
- */
+const middleware = t.middleware
+
+const isAuth = middleware(async (opts) => {
+   const session = await auth()
+
+    if (!session || !session.user || !session.user.id) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
+    }
+
+    return opts.next({
+        ctx: {
+            userId: session.user.id,
+            user: session.user,
+        },
+    })
+})
+
+
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const privateProcedure = t.procedure.use(isAuth)
