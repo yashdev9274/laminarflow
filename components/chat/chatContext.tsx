fe1c8b54365304899967161f7ae660ac37ce2cbb
yashdev9274 <yashdev.yvd@gmail.com>
2025-09-01 01:03:28 +0530

    Implement Message Sending Functionality in Chat Context: - Added a new API route for sending messages, including user authentication checks. - Integrated message sending logic within the ChatContext using React Query for asynchronous operations. - Enhanced state management for message input and loading status in the chat context provider.

--- components/chat/chatContext.tsx
+++ components/chat/chatContext.tsx
@@ -1,5 +1,6 @@
-import { ReactNode, createContext, useState } from "react";
+import React, { ReactNode, createContext, useState } from "react";
 import { useToast } from "../ui/use-toast";
+import { useMutation } from "@tanstack/react-query";
 
 type StreamResponse = {
    addMessage: ()=> void,
@@ -21,7 +22,47 @@ interface Props{
 }
 
 export const ChatContextProvider = ({fileId, children}: Props)=>{
-   const [message, setMesasge] = useState<string>('')
+   const [message, setMessage] = useState<string>('')
+   const[isLoading, setIsLoading] = useState<boolean>(false)
 
    const {toast} = useToast()
+
+   const {mutate: sendMessage} =  useMutation({
+            mutationFn: async ({ message }: { message: string }) => {
+         const response = await fetch('/api/message',{
+            method: 'POST',
+            body: JSON.stringify({
+               fileId,
+               message,
+            }),
+         })
+
+         if(!response.ok){
+            throw new Error('Failed to send message')
+         }
+         return response.body
+      }
+   })
+
+   const handleInputChange=(
+         e: React.ChangeEvent<HTMLTextAreaElement>
+      )=>{
+         setMessage(e.target.value)
+      }
+   
+
+   const addMessage = ()=> sendMessage({message})
+   
+   return(
+      <ChatContext.Provider
+         value={{
+            addMessage,
+            message,
+            handleInputChange,
+            isLoading,
+         }}
+      >
+         {children}
+      </ChatContext.Provider>
+   )
 }
