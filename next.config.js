793f08fea1a0a4e7c330bacbd5a28527b1330568
Sahil Chandel <rajputsahilchandel@gmail.com>
2025-10-24 23:12:58 +0530

    Feat: #153 added docker setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
   eslint: {
     ignoreDuringBuilds: true,
   },
